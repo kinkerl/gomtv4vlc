313ef06712be728da593a05da44b2ee6db57b9f2
Dennis Schwertel <s@digitalkultur.net>
2009-08-30 18:00:22 +0200

    initial

---
+++ etc/firefox-3.0/pref/gomtv4vlc.js
@@ -0,0 +1,6 @@
+pref("network.protocol-handler.app.gomcmd","/usr/bin/gomtv4vlc");
+pref("network.protocol-handler.external.gomcmd",true);
+pref("network.protocol-handler.warn-external.gomcmd",false);
+pref("network.protocol-handler.app.gomcmd+http","/usr/bin/gomtv4vlc");
+pref("network.protocol-handler.warn-external.gomcmd+http",false);
+
---
+++ usr/share/firefox/defaults/pref/gomtv4vlc.js
@@ -0,0 +1,5 @@
+pref("network.protocol-handler.app.gomcmd","/usr/bin/gomtv4vlc");
+pref("network.protocol-handler.warn-external.gomcmd",false);
+pref("network.protocol-handler.app.gomcmd+http","/usr/bin/gomtv4vlc");
+pref("network.protocol-handler.warn-external.gomcmd+http",false);
+
